3143fc69b84a2e6916d170c97f008c68fa8be99f
Dinesh Kumar <dinesh21dinu@gmail.com>
2020-03-30 22:16:06 +0530

    First commit

---
+++ MyFirstDemoLib/MyFirstDemoLib.h
@@ -0,0 +1,19 @@
+//
+//  MyFirstDemoLib.h
+//  MyFirstDemoLib
+//
+//  Created by Dinesh Thakur on 3/30/20.
+//  Copyright © 2020 Dinesh Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyFirstDemoLib.
+FOUNDATION_EXPORT double MyFirstDemoLibVersionNumber;
+
+//! Project version string for MyFirstDemoLib.
+FOUNDATION_EXPORT const unsigned char MyFirstDemoLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFirstDemoLib/PublicHeader.h>
+
+
